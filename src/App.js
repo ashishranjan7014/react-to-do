ebe5b8a3f146fa498074745638b2d4c60954dc03
ashishranjan7014 <ashishranjan7014@gmail.com>
2021-08-31 01:09:51 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux';
 import Store from './Store/Store';
 import ToDoList from './TodoList/TodoList';
 
-console.log(Store);
 export default function App() {
   return (
     <Provider store={Store}>
--- src/TodoList/TodoList.js
+++ src/TodoList/TodoList.js
@@ -8,17 +8,22 @@ const ToDoList = () => {
     <div>
       {toDoList && toDoList.length
         ? toDoList.map(({ email, name, contact }, index) => {
-            <div key={index + email}>
-              <div>
-                <strong>Name : </strong> {name}
+            return (
+              <div
+                style={{ margin: '1px', border: '2px solid black' }}
+                key={index + email}
+              >
+                <div style={{ padding: '10px' }}>
+                  <strong>Name : </strong> {name}
+                </div>
+                <div style={{ padding: '10px' }}>
+                  <strong>Email : </strong> {email}
+                </div>
+                <div style={{ padding: '10px' }}>
+                  <strong>Contact : </strong> {contact}
+                </div>
               </div>
-              <div>
-                <strong>Email : </strong> {email}
-              </div>
-              <div>
-                <strong>Contact : </strong> {contact}
-              </div>
-            </div>;
+            );
           })
         : null}
     </div>
