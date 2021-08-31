5e0f9c748a907c5fa78776d7e8effef6b1b5482c
ashishranjan7014 <ashishranjan7014@gmail.com>
2021-08-31 14:18:14 +0530

    1 file modified

--- src/Store/Reducers/ListReducer.js
+++ src/Store/Reducers/ListReducer.js
@@ -1,16 +1,32 @@
 import ActionTypes from '../Constant/Constant';
 
 const defaultState = {
-  toDoList: []
+  toDoList: [],
+  isEmailExists: []
 };
 const ListReducers = (state = defaultState, action) => {
   const { type, payload } = action;
   switch (type) {
     case ActionTypes.ADD_TO_LIST: {
-      return {
-        ...state,
-        toDoList: [...state.toDoList, payload]
-      };
+      if (!state.isEmailExists.includes(payload.email)) {
+        return {
+          ...state,
+          toDoList: [...state.toDoList, payload]
+        };
+      } else if (state.isEmailExists.includes(payload.email)) {
+        let toDoList = state.toDoList;
+        toDoList.map((e, i, a) => {
+          if (e.email === payload.email) {
+            a[i].email = payload.email;
+            a[i].name = payload.name;
+            a[i].contact = payload.contact;
+          }
+        });
+        return {
+          ...state,
+          toDoList
+        };
+      }
     }
     case ActionTypes.REMOVE_FROM_LIST: {
       const { id } = payload;
