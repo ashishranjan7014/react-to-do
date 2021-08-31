7f918ebde94e60896d2ca978447f00e62c1b65e2
ashishranjan7014 <ashishranjan7014@gmail.com>
2021-08-31 17:43:28 +0530

    1 file modified

--- src/Store/Action/FetchToDoList.js
+++ src/Store/Action/FetchToDoList.js
@@ -2,12 +2,12 @@ import ActionTypes from '../Constant/Constant';
 import Axios from 'axios';
 
 const FetchToDoList = () => {
-  Axios.get('node-a9gqtv--8080.local.webcontainer.io')
+  Axios.get('https://node-a9gqtv--8080.local.webcontainer.io/')
     .then(res => {
       console.log(res);
     })
     .catch(err => {
-      console.log(err);
+      // console.log(err);
     });
   return {
     type: ActionTypes.FETCH_TO_DO_LIST
