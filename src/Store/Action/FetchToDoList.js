af329b6c38b8f87a0cd4d35a16c842479417be66
ashishranjan7014 <ashishranjan7014@gmail.com>
2021-08-31 17:23:49 +0530

    1 file modified

--- src/Store/Action/FetchToDoList.js
+++ src/Store/Action/FetchToDoList.js
@@ -2,11 +2,9 @@ import ActionTypes from '../Constant/Constant';
 
 const FetchToDoList = () => {
   fetch('node-a9gqtv--8080.local.webcontainer.io')
-    .then(res => {
-      return res.json();
-    })
+    .then(res => res.json())
     .then(data => {
-      console.log(data);
+      console.log(data, '################');
     });
   return {
     type: ActionTypes.FETCH_TO_DO_LIST
